18b0e5cc24fdc498ad7a6da744dc3f704b68f196
nicolafelice <felicenicola99@hotmail.com>
2021-11-18 17:10:27 +0100

    add: show category in index posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,7 +9,7 @@
       <th scope="col">#</th>
       <th class="col-5" scope="col">title</th>
       <th scope="col">author</th>
-      <th scope="col">content</th>
+      <th scope="col">category</th>
       <th scope="col">actions</th>
       </tr>
     </thead>
@@ -19,9 +19,9 @@
       <th scope="row">{{$post['id']}}</th>
       <td class="h4">{{$post['title']}}</td>
       <td>{{$post['author']}}</td>
-      <td><div id="text_content">{{$post['content']}}</div></td>
+      <td>{{$post['category']['name'] ?? '--'}}</td>
       <td>
-        <a href="{{route("admin.posts.show", $post["id"])}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
+        <a href="{{route("admin.posts.show", $post["id"])}}"><button type="button" class="btn btn-primary">Visualizza</button></a><br>
         <a href="{{route("admin.posts.edit", $post["id"])}}"><button type="button" class="btn btn-secondary mt-2">Modifica</button></a>
       </td>
       </tr>
